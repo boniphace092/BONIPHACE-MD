3baffda3443dc67ca16f3ca38e8f60bccc517666
BONIPHACE <boniphacemalegesi@yahoo.com>
2024-07-16 21:49:33 +0300

    Initial commit

---
+++ commandes/repos.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://api.github.com/repos/Fred1e/LUCKY_MD';
+  const img = 'https://telegra.ph/file/59ead3c2b58ca1b07734d.jpg';
+
+  try {
+    const response = await fetch(githubRepo);
+    const data = await response.json();
+
+    if (data) {
+      const repoInfo = {
+        stars: data.stargazers_count,
+        forks: data.forks_count,
+        lastUpdate: data.updated_at,
+        owner: data.owner.login,
+      };
+
+      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+      const gitdata = `*hellow whatsaap user
+this is* *Luck_md V5.*\n get session id *by*, *pairing code*  https://lucky-md-v5-by-fredie-603474bf471d.herokuapp.com//
+
+🗼 *REPOSITORY:* ${data.html_url}
+✨ *STARS:* ${repoInfo.stars}
+🧧 *FORKS:* ${repoInfo.forks}
+📅 *RELEASE DATE:* ${releaseDate}
+🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
+👨‍💻 *OWNER:* *Fredie tech*
+__________________________________
+            *Made With Fredie Tech*`;
+
+      await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+    } else {
+      console.log("Could not fetch data");
+    }
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+});
